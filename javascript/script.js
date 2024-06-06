9349383be9412b7982f1e7560efbbdd49c4ad0ce
AngelSulochana <90322278+AngelSulochana@users.noreply.github.com>
2024-06-06 20:54:58 +0000

    to print a list

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+var groceries = ['Milk', 'Bread', 'Egg']
+
+for (var i=0; i <groceries.length, i++){
+    console.log(groceries[i])
+}
--- javascript/test.js
+++ javascript/test.js
@@ -2,4 +2,9 @@ var number = 10;
 var string = 'Hello There';
 var isRad = true;
 
+if(number == 10){
+    console.log("yes Buddy")
+} else {
+    console.log("Nope!")
+}
 document.getElementById('box').innerHTML = string
