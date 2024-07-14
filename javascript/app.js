86f8ef49b45b0d3337a2cd7c464b332025b91cbc
AngelSulochana <90322278+AngelSulochana@users.noreply.github.com>
2024-07-14 16:57:47 +0000

    value to price

--- javascript/app.js
+++ javascript/app.js
@@ -11,14 +11,13 @@ function calculateShippingCost(totalpriceparam) {
         Shippingcost = 0
     }
 
-
-    //inital shopping
-    totalprice = 10;
-    //more shopping
-    totalprice = 19;
-
     console.log(`Your shipping cost is ${Shippingcost}`)
     console.log(`For a cart value of ${totalpriceparam}`)
 }
+
+//inital shopping
+totalprice = 10;
+//more shopping
+totalprice = 19;
 
 calculateShippingCost(totalprice)
